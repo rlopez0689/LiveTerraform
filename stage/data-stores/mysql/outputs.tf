f6eb9def153b6ab28d9b9568f1a523e89821b4c6
Rodrigo Lopez <rodrigo.lopez@digitalonus.com>
2018-10-16 12:08:47 -0500

    Add vpn cidr output

--- stage/data-stores/mysql/outputs.tf
+++ stage/data-stores/mysql/outputs.tf
@@ -4,7 +4,9 @@ output "address" {
 output "port" {
       value = "${aws_db_instance.example.port}"
 }
-
 output "vpc_id" {
     value = "${aws_vpc.stage-vpc.id}"
+}
+output "vpc_cidr_block" {
+    value = "${aws_vpc.stage-vpc.cidr_block}"
 }
